2f8e169b7386af54ca2a83239c9a2dfb12ef02ac
9naqmuame <9naquame@gmail.com>
2021-04-28 07:07:03 -0230

    added db and fix for os paths in config

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-from os import environ
+from os import environ, path
+
+basedir = path.abspath(path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = environ.get("SECRET_KEY") or "secret_key"
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI") or \
+        f"sqlite:///{path.join(basedir, 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
