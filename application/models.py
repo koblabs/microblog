8313c541c0cac30650fd206e7791e1d0cc7278f6
9naqmuame <9naquame@gmail.com>
2021-04-29 06:55:44 -0230

    added CRUD as well as created and updated dates to database

--- application/models.py
+++ application/models.py
@@ -1,24 +1,77 @@
 from datetime import datetime
+from hashlib import md5
 
-from application import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from application import db, login
 
 
-class  User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class CRUDMixin(object):
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+    def reload(self):
+        db.session.refresh(self)
+
+
+class CreateUpdateTimesMixin(object):
+    created_on = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime, index=True, default=datetime.utcnow,
+                           onupdate=datetime.utcnow)
+
+
+class  User(UserMixin, CRUDMixin, CreateUpdateTimesMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(64))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.username}>"
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-class Post(db.Model):
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+
+
+class Post(CRUDMixin, CreateUpdateTimesMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+
