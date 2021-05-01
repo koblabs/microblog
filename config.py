96f0403f6f6d2a78577d7de05909a12ddac5b800
9naqmuame <9naquame@gmail.com>
2021-05-01 02:09:03 -0230

    added posts per page

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI") or \
         f"sqlite:///{path.join(basedir, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = environ.get("MAIL_SERVER")
     MAIL_PORT = int(environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
     ADMINS = environ.get("MAIL_ADMINS") or ["koowusuboaky@gmail.com"]
+
+    POSTS_PER_PAGE = 10
