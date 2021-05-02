049395dfdb9311f9efd8367f5c08092171966612
9naqmuame <9naquame@gmail.com>
2021-05-02 08:41:40 -0230

    flask_bootstrap setup

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from logging.handlers import  SMTPHandler, RotatingFileHandler
 
 from config import Config
@@ -18,6 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login.login_view = "login"
 
