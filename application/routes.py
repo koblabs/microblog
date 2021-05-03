1e6b62d085ddec1d3d6b5d8259fa912c1479899f
9naqmuame <9naquame@gmail.com>
2021-05-03 12:09:12 -0230

    initiation of UI translate button

--- application/routes.py
+++ application/routes.py
@@ -29,8 +29,8 @@ def index():
     form = PostForm()
     if form.validate_on_submit():
         language = guess_language(form.post.data)
-        if language == 'UNKNOWN' or len(language) > 5:
-            language = ''
+        if language == "UNKNOWN" or len(language) > 5:
+            language = ""
         post = Post(body=form.post.data, author=current_user,
                     language=language)
         post.save()
