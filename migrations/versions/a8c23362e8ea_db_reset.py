24908d13daa8635a2e1e560686fee1aad87d9370
9naqmuame <9naquame@gmail.com>
2021-05-03 12:04:00 -0230

    db reset

--- migrations/versions/a8c23362e8ea_db_reset.py
+++ migrations/versions/a8c23362e8ea_db_reset.py
@@ -1,8 +1,8 @@
-"""new database structure
+"""db reset
 
-Revision ID: 5d8768e4efa7
+Revision ID: a8c23362e8ea
 Revises: 
-Create Date: 2021-04-28 12:34:00.856247
+Create Date: 2021-05-03 11:56:54.318626
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5d8768e4efa7'
+revision = 'a8c23362e8ea'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -25,18 +25,27 @@ def upgrade():
     sa.Column('username', sa.String(length=16), nullable=True),
     sa.Column('email', sa.String(length=120), nullable=True),
     sa.Column('password', sa.String(length=64), nullable=True),
+    sa.Column('about_me', sa.String(length=140), nullable=True),
+    sa.Column('last_seen', sa.DateTime(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_created_on'), 'user', ['created_on'], unique=False)
     op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=True)
     op.create_index(op.f('ix_user_updated_on'), 'user', ['updated_on'], unique=False)
     op.create_index(op.f('ix_user_username'), 'user', ['username'], unique=True)
+    op.create_table('followers',
+    sa.Column('follower_id', sa.Integer(), nullable=True),
+    sa.Column('following_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['follower_id'], ['user.id'], ),
+    sa.ForeignKeyConstraint(['following_id'], ['user.id'], )
+    )
     op.create_table('post',
     sa.Column('created_on', sa.DateTime(), nullable=True),
     sa.Column('updated_on', sa.DateTime(), nullable=True),
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('body', sa.String(length=140), nullable=True),
     sa.Column('user_id', sa.Integer(), nullable=True),
+    sa.Column('language', sa.String(length=5), nullable=True),
     sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
@@ -50,6 +59,7 @@ def downgrade():
     op.drop_index(op.f('ix_post_updated_on'), table_name='post')
     op.drop_index(op.f('ix_post_created_on'), table_name='post')
     op.drop_table('post')
+    op.drop_table('followers')
     op.drop_index(op.f('ix_user_username'), table_name='user')
     op.drop_index(op.f('ix_user_updated_on'), table_name='user')
     op.drop_index(op.f('ix_user_email'), table_name='user')
