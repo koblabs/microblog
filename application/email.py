8fff45843614407ef41638a55253b41c0f68a776
9naqmuame <9naquame@gmail.com>
2021-05-10 08:13:05 -0230

    updated emailing

--- application/email.py
+++ application/email.py
@@ -12,11 +12,16 @@ def send_async_email(app, message):
         mail.send(message)
 
 
-def send_mail(subject, sender, recipients, body, html):
+def send_mail(subject, sender, recipients, body, html,
+              attachments=None, sync=False):
     message = Message(subject, sender=sender, recipients=recipients)
     message.body = body
     message.html = html
-    # mail.send(message)
-    # print(body)
-    Thread(target=send_async_email, 
-            args=(current_app._get_current_object(), message)).start()
+    if attachments:
+        for attachment in attachments:
+            message.attach(*attachment)
+    if sync:
+        mail.send(message)
+    else:
+        Thread(target=send_async_email, 
+                args=(current_app._get_current_object(), message)).start()
