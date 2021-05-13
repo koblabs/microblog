cacd693889f42ff0bc931dd615d6345198d8cff3
9naqmuame <9naquame@gmail.com>
2021-05-12 23:34:37 -0230

    api tokens added

---
+++ application/api/tokens.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from flask import jsonify
+
+from application import db
+from application.api import bp
+from application.api.auth import basic_auth, token_auth
+
+
+@bp.route("/tokens", methods=["POST"])
+@basic_auth.login_required
+def get_token():
+    token = basic_auth.current_user().get_token()
+    db.session.commit()
+    return jsonify({"token": token})
+
+
+@bp.route("/tokens", methods=["DELETE"])
+@token_auth.login_required
+def revoke_token():
+    token_auth.current_user().revoke_token()
+    db.session.commit()
+    return "", HTTPStatus.NO_CONTENT
