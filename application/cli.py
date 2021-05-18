57c3bc18b1393e8d9ebfbe6fd9d26edbcb83f257
9naqmuame <9naquame@gmail.com>
2021-05-18 11:45:24 -0230

    language support fixes

--- application/cli.py
+++ application/cli.py
@@ -18,7 +18,7 @@ def register(app):
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
         if os.system(
-                'pybabel init -i messages.pot -d app/translations -l ' + lang):
+                'pybabel init -i messages.pot -d application/translations -l ' + lang):
             raise RuntimeError('init command failed')
         os.remove('messages.pot')
 
@@ -28,7 +28,7 @@ def register(app):
         """Update all languages."""
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
-        if os.system('pybabel update -i messages.pot -d app/translations'):
+        if os.system('pybabel update -i messages.pot -d application/translations'):
             raise RuntimeError('update command failed')
         os.remove('messages.pot')
 
@@ -36,5 +36,5 @@ def register(app):
     @translate.command()
     def compile():
         """Compile all languages."""
-        if os.system('pybabel compile -d app/translations'):
+        if os.system('pybabel compile -d application/translations'):
             raise RuntimeError('compile command failed')
