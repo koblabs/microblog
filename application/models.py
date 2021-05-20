d4dd8c28b22ab5eaf958a2691c717912e9d7fdb9
9naqmuame <9naquame@gmail.com>
2021-05-20 04:07:31 -0230

    updated models to reflect changes in model setup in relation to mixins

--- application/models.py
+++ application/models.py
@@ -10,6 +10,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from application import db, login
 from application.search import add_to_index, remove_from_index, query_index
+from application.model.api import PaginationMixin
+from application.model.base import CRUDMixin, CreateUpdateTimesMixin, \
+    SearchableMixin
 
 
 followers = db.Table("followers",
@@ -23,102 +26,8 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class CRUDMixin(object):
-    @classmethod
-    def create(cls, **kwargs):
-        instance = cls(**kwargs)
-        return instance.save()
-
-    def update(self, commit=True, **kwargs):
-        for attr, value in kwargs.items():
-            setattr(self, attr, value)
-        return commit and self.save() or self
-
-    def save(self, commit=True):
-        db.session.add(self)
-        if commit:
-            db.session.commit()
-        return self
-
-    def delete(self, commit=True):
-        db.session.delete(self)
-        return commit and db.session.commit()
-
-    def reload(self):
-        db.session.refresh(self)
-
-
-class CreateUpdateTimesMixin(object):
-    created_on = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    updated_on = db.Column(db.DateTime, index=True, default=datetime.utcnow,
-                           onupdate=datetime.utcnow)
-
-
-class SearchableMixin(object):
-    @classmethod
-    def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename__, expression, page, per_page)
-        if total == 0:
-            return cls.query.filter_by(id=0), 0
-        when = []
-        for i in range(len(ids)):
-            when.append((ids[i], i))
-        return cls.query.filter(cls.id.in_(ids)).order_by(
-            db.case(when, value=cls.id)), total
-
-    @classmethod
-    def before_commit(cls, session):
-        session._changes = {
-            "add": list(session.new),
-            "update": list(session.dirty),
-            "delete": list(session.deleted)
-        }
-
-    @classmethod
-    def after_commit(cls, session):
-        for obj in session._changes["add"]:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes["update"]:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__. obj)
-        for obj in session._changes["delete"]:
-            if isinstance(obj, SearchableMixin):
-                remove_from_index(obj.__tablename__, obj)
-        session._changes = None
-
-    @classmethod
-    def reindex(cls):
-        for obj in cls.query:
-            add_to_index(cls.___tablename__, obj)
-
-
-class PaginatedAPIMixin(object):
-    @staticmethod
-    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
-        resources = query.paginate(page, per_page, False)
-        data = {
-            "items": [item.to_dict() for item in resources.items],
-            "_meta": {
-                "page": page,
-                "per_page": per_page,
-                "total_pages": resources.pages,
-                "total_items": resources.total
-            },
-            "_links": {
-                "self": url_for(endpoint, page=page, per_page=per_page,
-                                **kwargs),
-                "next": url_for(endpoint, page=page + 1, per_page=per_page,
-                                **kwargs) if resources.has_next else None,
-                "prev": url_for(endpoint, page=page - 1, per_page=per_page,
-                                **kwargs) if resources.has_prev else None
-            }
-        }
-        return data
-
-
 class  User(UserMixin, CRUDMixin, CreateUpdateTimesMixin,
-            PaginatedAPIMixin, db.Model):
+            PaginationMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
